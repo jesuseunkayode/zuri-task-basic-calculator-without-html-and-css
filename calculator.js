d7144bfc1296200af3196e2d16e2957458c76f6d
jesuseunkayode <kayode.jesuseun@hotmail.com>
2022-06-20 23:10:52 +0100

    ready

--- calculator.js
+++ calculator.js
@@ -1,19 +1,13 @@
-let firstNumber = prompt("Enter first number:   "),
-    operator = prompt("operator:  "),
-    secondNumber = prompt("Enter second number:  ");
+let firstNumber = Number.parseInt(prompt("Enter first number:   "), 10),
+  operator = prompt("Enter the arithmetic operator:  "),
+  secondNumber = Number.parseInt(prompt("Enter second number:  "), 10);
 
-    if(operator ==="+") {
-        alert(firstNumber + secondNumber);
-    } 
-    
-    else if (operator ==="-") {
-        alert(firstNumber - secondNumber);
-    } 
-    
-    else if(operator ==="*") {
-        alert(firstNumber * secondNumber);
-    } 
-    
-     else if(operator ==="/") {
-        alert(firstNumber / secondNumber);
-    }
+if (operator === "+") {
+  alert(firstNumber + secondNumber);
+} else if (operator === "-") {
+  alert(firstNumber - secondNumber);
+} else if (operator === "*") {
+  alert(firstNumber * secondNumber);
+} else if (operator === "/") {
+  alert(firstNumber / secondNumber);
+}
